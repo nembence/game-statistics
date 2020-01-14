1401f73975ed8fb9c38afdce1d6f60028ad5701a
nembence <nemeth.bence@icloud.com>
2020-01-14 15:52:28 +0100

    add reports and printing modules

--- printing.py
+++ printing.py
@@ -1,2 +1,56 @@
+import reports
+import os 
 
-# Printing functions
+
+def questions():
+    print("1. How many games are in the file?")
+    print("2. Is there a game from a given year?")
+    print("3. Which is the latest game?")
+    print("4. How many games are in the file by genre?")
+    print("5. What is the line number of a given title?")
+    print("6. Can you give me the alphabetically ordered list of the titles?")
+    print("7. Which genres occur in the data file?")
+    print("8. What is the release year of the top sold first-person shooter game?")
+    question = int(input("Which question do you want to pick: "))
+    return question
+
+def functions(question,filename):
+    if question == 1:
+        print(reports.count_games(filename))    
+    elif question == 2:
+        year=int(input("What is the given year: "))
+        print(reports.decide(filename,year))
+    elif question == 3:
+        print(reports.get_latest(filename))
+    elif question == 4:
+        genre=input("Which genre do you want to check: ")
+        print(reports.count_by_genre(filename,genre))
+    elif question == 5:
+        title=input("What title do you want to check: ")
+        print(reports.get_line_number_by_title(filename,title))
+    elif question == 6:
+        print(reports.sort_abc(filename))
+    elif question == 7:
+        print(reports.get_genres(filename))
+    elif question == 8:
+        print(reports.when_was_top_sold_fps(filename))
+    
+def ask():
+    back = input("Do you want to ask again (y/n): ")
+    if back == "y":
+        return True
+    else:
+        return False 
+
+def main():
+    filename=input("What is your file name: ")
+    os.system("clear")
+    asking = True
+    while asking is True:
+        os.system("clear")
+        question = questions()
+        functions(question,filename)
+        asking = ask()
+
+if __name__ == '__main__':
+    main()
--- reports.py
+++ reports.py
@@ -1,11 +1,11 @@
 def count_games(file_name):
-    with open(file_name,'r') as f:
-        return len(f.readlines())
+    with open(file_name,'r') as file:
+        return len(file.readlines())
 
 
 def decide(file_name,year):
-    with open(file_name,'r') as f:
-        games=f.readlines()
+    with open(file_name,'r') as file:
+        games=file.readlines()
         for i in games:
             if str(year)in i:
                 return True
@@ -13,8 +13,58 @@ def decide(file_name,year):
     
 
 def get_latest(file_name):
-    with open(file_name,'r') as f:
-        games=sorted([line.split('\t') for line in f.readlines()], key = lambda  x : x[2])
+    with open(file_name,'r') as file:
+        games=sorted([line.split('\t') for line in file.readlines()], key = lambda  x : x[2])
         return games[-1][0]
 
-print(get_latest('game_stat.txt'))
+
+def count_by_genre(file_name, genre):
+    with open(file_name,'r') as file:
+        games=[line.split('\t') for line in file.readlines()]
+        count=0
+        for game in games:
+            if game[3] == genre:
+                count += 1
+        return count        
+       
+
+def get_line_number_by_title(file_name,title):
+    with open(file_name,'r') as file:
+        games=[line.split('\t') for line in file.readlines()]
+        for game in range(len(games)):
+            if games[game][0] == title:
+                return game+1
+        raise ValueError()        
+
+def sort_abc(file_name):
+    with open(file_name,'r') as file:
+        titles=[line.split('\t')[0] for line in file.readlines()]
+        for title in range(len(titles)-1,0,-1):
+            for j in range(title):
+                if titles[j] > titles[j+1]:
+                    temp = titles[j+1]
+                    titles[j+1] = titles[j]
+                    titles[j] = temp
+        return titles
+
+def get_genres(file_name):
+    with open(file_name,'r') as file:
+        genres=[line.split('\t')[3] for line in file.readlines()]
+        genres=list(set(genres))
+        for genre in range(len(genres)-1,0,-1):
+            for j in range(genre):
+                if genres[j] > genres[j+1]:
+                    temp = genres[j+1]
+                    genres[j+1] = genres[j]
+                    genres[j] = temp
+    return genres
+
+def when_was_top_sold_fps(file_name):
+    with open(file_name,'r') as file:
+        fps = sorted([line.split('\t') for line in file.readlines() if line.split('\t')[3] == 'First-person shooter'], key = lambda x:float(x[1]))
+        if len(fps) == 0:
+            raise ValueError()
+        else:
+            return int(fps[-1][2])
+
+
